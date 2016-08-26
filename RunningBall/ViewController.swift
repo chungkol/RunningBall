e460c010551295d4b476beee839c5fafcba7eb44
chung <chungvtgc00989@fpt.edu.vn>
2016-08-26 11:52:33 +0700

    basic

--- RunningBall/ViewController.swift
+++ RunningBall/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var ball = UIImageView()
+    var radians = CGFloat()
+    var ballRadius = CGFloat()
+    var isEnd: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addBall()
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector(rollBall), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addBall(){
+        let mainViewSize = self.view.bounds.size
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ballRadius = 32.0
+        ball.center = CGPointMake(ballRadius, mainViewSize.height/2)
+        self.view.addSubview(ball)
     }
-
-
+    
+    func rollBall(){
+        let deltaAngle: CGFloat = 0.1
+        radians = radians + deltaAngle
+        let positionBall = self.ball.center.x
+        if !isEnd {
+            ball.transform = CGAffineTransformMakeRotation(radians)
+            ball.center = CGPointMake(positionBall + ballRadius * deltaAngle, ball.center.y)
+        }else{
+            ball.transform = CGAffineTransformMakeRotation(-radians)
+            ball.center = CGPointMake(ball.center.x - ballRadius * deltaAngle, ball.center.y)
+        }
+        if Int(positionBall) >= Int(self.view.bounds.width - (self.ball.bounds.width / 2)) {
+            isEnd = true
+            print("trung nhau \(Int(self.view.bounds.width))")
+        }
+        if Int(positionBall) <= 15 {
+            isEnd = false
+           
+        }
+        
+    }
 }
 
